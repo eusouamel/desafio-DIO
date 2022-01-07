de1b7eb7a222c41412cd908faebc8b78cd59a7eb
eusouamel <nogueira.mel44@gmail.com>
2022-01-06 23:15:54 -0300

    Incluindo programação Tabuada em C#

---
+++ Tabuada_atividade/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Tabuada_atividade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstTabuada = new System.Windows.Forms.ListBox();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnTabuada = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstTabuada
+            // 
+            this.lstTabuada.FormattingEnabled = true;
+            this.lstTabuada.Location = new System.Drawing.Point(278, 37);
+            this.lstTabuada.Name = "lstTabuada";
+            this.lstTabuada.Size = new System.Drawing.Size(130, 342);
+            this.lstTabuada.TabIndex = 0;
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(12, 37);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(95, 13);
+            this.lblNumero.TabIndex = 1;
+            this.lblNumero.Text = "Digite um número: ";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(113, 37);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // btnTabuada
+            // 
+            this.btnTabuada.Location = new System.Drawing.Point(79, 137);
+            this.btnTabuada.Name = "btnTabuada";
+            this.btnTabuada.Size = new System.Drawing.Size(102, 46);
+            this.btnTabuada.TabIndex = 3;
+            this.btnTabuada.Text = "Gerar Tabuada";
+            this.btnTabuada.UseVisualStyleBackColor = true;
+            this.btnTabuada.Click += new System.EventHandler(this.btnTabuada_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 450);
+            this.Controls.Add(this.btnTabuada);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.lstTabuada);
+            this.Name = "Form1";
+            this.Text = "Tabuada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstTabuada;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnTabuada;
+    }
+}
+
---
+++ Tabuada_atividade/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabuada_atividade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTabuada_Click(object sender, EventArgs e)
+        {
+            lstTabuada.Items.Clear();
+            int num, tab, cont;
+
+            num = int.Parse(txtNumero.Text);
+
+            for (cont = 1; cont <= 10; cont++)
+            {
+                tab = num * cont;
+                lstTabuada.Items.Add(num + " X " + cont + " = " + tab);
+            }
+        }
+    }
+}
